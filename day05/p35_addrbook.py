c007afd6483a4064ef2706de0bacb4990511a36a
zzzissu <gkswltn1223@naver.com>
2024-02-27 11:46:52 +0900

    24.02.27

--- day05/p35_addrbook.py
+++ day05/p35_addrbook.py
@@ -1,6 +1,8 @@
 # file : p35_addrbook.py
 # desc : 콘솔 주소록 프로그램
 
+import os
+
 class Contact:  # 주소록 클래스
     # __name =''
     # __phoneNumber = ''
@@ -21,12 +23,14 @@ class Contact:  # 주소록 클래스
         return res
     
 def setContact():   # 사용자 입력으로 주소록 받기 함수
-    (name, phoneNumber, eMail, addr) = input('주소록 입력(이름, 핸드폰, 이메일, 주소[구분자 / ]) > ').split('/')
+    (name, phoneNumber, eMail, addr) = input('입력(이름, 핸드폰, 이메일, 주소[/]) > ').split('/')
     name = name.strip()         # strip = 사용자로 인한 앞, 뒤 스페이스 공백을 제거
     phoneNumber = phoneNumber.strip()
     eMail = eMail.strip()
     addr = addr.strip()
-    print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+    # print(f'"{name}", "{phoneNumber}", "{eMail}", "{addr}"')
+    contact = Contact(name, phoneNumber, eMail, addr)   # 매개변수 명과 동일하게 로컬변수 이름 지정
+    return contact
     
 def displayManu():
     menu = ('주소록 프로그램 \n'
@@ -35,20 +39,47 @@ def displayManu():
             '3. 연락처 삭제 \n'
             '4. 종료')
     print(menu)
-    sel = input('메뉴입력 : ')
+    sel = int(input('메뉴입력 : '))
     return sel
+
+def clearConsole():
+    cmd = 'clear'   #mac OD, Linux, Unix 명령어
+    if os.name in ('nt', 'dos'):    #window
+        cmd = 'cls' #window 명령어
+        
+    os.system(cmd)  # 명령어 실행
+    
+def getContacts(list):  # 리스트를 받아서 출력함수
+    for item in list:
+        print(item)
     
 def run():
+    # 연락처를 담을 주소록 리스트 생성
+    listContact = []
+    
+    clearConsole()  # 화면을 클리어
     while True:
         selMenu = displayManu()
-        
-        if selMenu == 4:
+        if selMenu == 1:    # 연락처 추가인 경우
+            clearConsole()
+            contact = setContact()
+            listContact.append(contact)
+            # print(listContact)
+            input() # 엔터를 입력하도록 유도
+            clearConsole()
+        elif selMenu == 2:  # 연락처 출력
+            clearConsole()
+            getContacts(listContact)
+            input()
+            clearConsole()
+        elif selMenu == 4:
             break
+        else:
+            clearConsole()
     # setContact()
     # first = Contact('홍길동', '010-8532-4832', 'fswe@naver.com', '경성')      # 위의 순서와 맞게 입력
     # first = Contact(addr='경성', phoneNumber='010-8532-4832', name='홍길동', eMail='fswe@naver.com')
     # print(first)
-
     
 if __name__ == '__main__':  # 메인트리
     print('프로그램 시작')
